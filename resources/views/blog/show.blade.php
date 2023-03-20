a224ba2fce1f51285101e90b998ed2b43ae0ee32
Francois Poulin <poulinfrancois77@gmail.com>
2023-03-20 13:06:42 -0400

    Ajout modal de suppression

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -17,35 +17,31 @@
     <hr>
     <div class="row text-center">
         <div class="col-md-6">
-            <a href="{{ route('blog.edit', $blogPost->id) }}" class="btn btn-success"> Modifier </a>
+            <a href="{{ route('blog.edit', $blogPost->id) }}" class="btn btn-sm btn-success"> Modifier </a>
         </div>
         <div class="col-md-6">
-        <form  method="post">
-        @csrf
-        @method('delete')
-            <input type="submit" value="Delete" class="btn btn-danger btn-sm">
-        </form>
+            <input type="buttom" value="Delete" data-bs-toggle="modal" data-bs-target="#modalDelete" class="btn btn-danger btn-sm">
         </div>
     </div>
-    <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Launch demo modal
-</button>
 
 <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="modalDelete" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Supprimation?</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        ...
+        Do you sure to want to destroy the clicked on the supprimé de la chose? {{ $blogPost->title }}
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Oui</button>
+        <form  method="post">
+        @csrf
+        @method('delete')
+            <input type="button" value="Delete"class="btn btn-danger">
+        </form>
       </div>
     </div>
   </div>
